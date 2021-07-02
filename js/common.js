143116c918bd8e70239943366682820b8b940205
jye104 <jye104@nate.com>
2021-07-02 13:52:29 +0900

    20210702 13:52
    중간 작업 내용

--- js/common.js
+++ js/common.js
@@ -1,67 +1,79 @@
 $(document).ready(function () {
   // pcHeader.on
-  $(window).on('scroll', function(){
-    if ($(this).scrollTop()>10) $('#pcHeader').removeClass('on').addClass('beige');
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 10) $('#pcHeader').removeClass('on').addClass('beige');
     else $('#pcHeader').addClass('on').removeClass('beige');
   });
   // mHeader.on
-  $(window).on('scroll', function(){
-    if ($(this).scrollTop()>10) $('#mHeader').addClass('beige');
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 10) $('#mHeader').addClass('beige');
     else $('#pcHeader').removeClass('beige');
   });
-
   // pcGnb
   const $gnb = $('#pcGnb > ul');
   const $gnbD2 = $gnb.find('li ul');
   const $gnbD2a = $gnb.find('li ul li a');
-
   $gnbD2.hide();
-
-  $gnb.children().on('mouseenter focusin', function(){
+  $gnb.children().on('mouseenter focusin', function () {
     $(this).addClass('on').siblings('li.on').removeClass('on');
     $('#pcHeader').addClass('active');
     $gnbD2.stop().slideDown();
-   });
-  $gnb.on('mouseleave', function(){
-    $(this).children().removeClass('on').children('ul').stop().slideUp('fast', function(){
+  });
+  $gnb.on('mouseleave', function () {
+    $(this).children().removeClass('on').children('ul').stop().slideUp('fast', function () {
       $('#pcHeader').removeClass('active').find('#gnb').removeClass('on');
     });
   });
 
   // mGnb
-  $('#mGnb ul li a').on('click', function(){
-    if ($(this).next().length === 0){ 
-       return true; 
+  $('#mGnb ul li a').on('click', function () {
+    if ($(this).next().length === 0) {
+      return true;
     } else {
-       const liHei = $(this).next().children().outerHeight(true);
-       const liSize = $(this).next().children().length;
-       const ulHei = liHei * liSize;
+      const liHei = $(this).next().children().outerHeight(true);
+      const liSize = $(this).next().children().length;
+      const ulHei = liHei * liSize;
 
-       $(this).parent().siblings().removeClass('on').children('ul').stop().animate({maxHeight: 0}, function(){
-           $(this).css({visibility: 'hidden'});
-       });
+      $(this).parent().siblings().removeClass('on').children('ul').stop().animate({
+        maxHeight: 0
+      }, function () {
+        $(this).css({
+          visibility: 'hidden'
+        });
+      });
 
-       if ($(this).parent().hasClass('on')) { 
-           $(this).next().animate({maxHeight: 0}, function(){
-               $(this).css({visibility: 'hidden'});
-           }).parent().removeClass('on');
-       } else{ 
-           $(this).next().css('visibility', 'visible').stop().animate({maxHeight: ulHei}).parent().addClass('on');
-       }
+      if ($(this).parent().hasClass('on')) {
+        $(this).next().animate({
+          maxHeight: 0
+        }, function () {
+          $(this).css({
+            visibility: 'hidden'
+          });
+        }).parent().removeClass('on');
+      } else {
+        $(this).next().css('visibility', 'visible').stop().animate({
+          maxHeight: ulHei
+        }).parent().addClass('on');
+      }
     }
     return false;
-});
+  });
   const $mGnb = $('#mHeader #mGnb');
   $('#mHeader .btn_gnb_open').on('click', function () {
-    const $first =  $mGnb.find('.first');
-    const $last =  $mGnb.find('.last');
+    const $first = $mGnb.find('.first');
+    const $last = $mGnb.find('.last');
     const $openBtn = $(this);
-    $mGnb.css('visibility', 'visible').stop().animate({left: 0}, 300, function () {
-      $first.focus(); 
-      $('html').css({overflowY: 'hidden', height: '100%'});
-  });
+    $mGnb.css('visibility', 'visible').stop().animate({
+      left: 0
+    }, 300, function () {
+      $first.focus();
+      $('html').css({
+        overflowY: 'hidden',
+        height: '100%'
+      });
+    });
     $first.on('keydown', function (e) {
-      if ( e.shiftKey && e.keyCode === 9 ) {
+      if (e.shiftKey && e.keyCode === 9) {
         e.preventDefault();
         $last.focus();
       }
@@ -79,46 +91,58 @@ $(document).ready(function () {
         $(this).remove();
       });
 
-      $mGnb.stop().animate({left: '100%'}, 300, function () {
+      $mGnb.stop().animate({
+        left: '100%'
+      }, 300, function () {
         $(this).css('visibility', 'hidden');
         $openBtn.focus();
 
-        $('#mGnb ul li.on').removeClass('on').children('ul').css({visibility: 'hidden', maxHeight: 0});
-        });      
+        $('#mGnb ul li.on').removeClass('on').children('ul').css({
+          visibility: 'hidden',
+          maxHeight: 0
+        });
+      });
     });
-});
-
-
+  });
 
   // dep2 a:hover, a:focus 제어
   const $gnbLine = $('#gnbLine');
-  $gnbD2a.on('mouseenter focusin', function(){
+  $gnbD2a.on('mouseenter focusin', function () {
     $(this).after($gnbLine);
   });
-  $gnbD2a.on('mouseleave', function(){
+  $gnbD2a.on('mouseleave', function () {
     $(this).siblings($gnbLine).remove();
   });
   // gnb키보드 제어
-  $gnb.find('a').first().on('keydown', function(e){
-    if((e.shiftKey || e.keyCode === 16) && e.keyCode === 9) $gnb.trigger('mouseleave');
+  $gnb.find('a').first().on('keydown', function (e) {
+    if ((e.shiftKey || e.keyCode === 16) && e.keyCode === 9) $gnb.trigger('mouseleave');
   });
-  $gnb.find('a').last().on('keydown', function(e){
-    if((!e.shiftKey || e.keyCode !==16) && e.keyCode ===9) $gnb.trigger('mouseleave');
+  $gnb.find('a').last().on('keydown', function (e) {
+    if ((!e.shiftKey || e.keyCode !== 16) && e.keyCode === 9) $gnb.trigger('mouseleave');
   });
+
+
+  // fade 효과
+  $(window).on('scroll', function () {
+    var scrollY = $(this).scrollTop() + $(this).height() * 2 / 3;
+    console.log(scrollY);
+
+    $('.fade').each(function (idx) {
+      if (scrollY > $(this).offset().top) {
+        $(this).addClass('on');
+      } else { //반복적인 효과가 필요한 경우만 사용하기
+        $(this).removeClass('on');
+      }
+    });
+  });
+
+  $(window).trigger('scroll');
+
+  $(".btn_top").on("click", function () {
+    $("html, body").stop().animate({
+      scrollTop: 0
+    });
+    return false;
+  });
+
 });
-
-// fade 효과
-$(window).on('scroll', function () {
-  var scrollY = $(this).scrollTop() + $(this).height() * 2/3; 
-  console.log(scrollY);
-
-  $('.fade').each(function (idx) {
-    if (scrollY > $(this).offset().top) {
-      $(this).addClass('on');
-    } else { //반복적인 효과가 필요한 경우만 사용하기
-      $(this).removeClass('on');
-    }
-  });
-});
-
-$(window).trigger('scroll');
